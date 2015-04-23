c61a22ae3b990b791992abd0900a3bfaf99e9846
Harry <h.l.schroter@gmail.com>
2015-04-23 14:19:18 +0200

    Tiny adjustments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $(function()
 {
 	var model = new EnergyClockModel();	
-
 	var clockView = new ClockView("#energyClock", model);
 	var clockController = new ClockController(clockView, model);
 
 	/********************************************************************************
+					 			Vertical Centering
+	********************************************************************************/
+
+	$(window).resize(function(){
+		verticalCenter();
+	});
+
+	var verticalCenter = function(){
+		var screenHeight = $(window).height();
+		$("#energyClock").css('margin-top',( screenHeight-770)/2 );
+	}
+
+	verticalCenter();
+
+	/********************************************************************************
 					 	Showing / Hiding the information screen
 	********************************************************************************/
+
 	var height = $("#information").outerHeight(true);
 	$("#information").css('bottom',-height);
 	$("#hide").click(function(event) {
